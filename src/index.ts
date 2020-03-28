e1c62c5a66c8d2e54b8a4bc0084c37d38e1ab933
DOGUKKIM <59882993+Moderatio@users.noreply.github.com>
2020-03-28 16:23:52 +0900

    make a blockChain

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import CryptoJS from "crypto-js";
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const haLim = new Human("lim", 25, "female");
+const genesisBlock: Block = new Block(0, "lalal", "", "hello", 123456);
+let blockchain: Block[] = [genesisBlock];
+const getBlockChain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-const nico = {
-  name: "nico",
-  age: 44,
-  gender: "male"
-};
-
-const sayHi = (person: Human): string => {
-  return `hey ${person.name} u r ${person.age} and ${person.gender}!`;
-};
-
-console.log(sayHi(haLim));
 export {};
